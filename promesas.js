3cbd9090630de7b0f079ec99691932a8637886d1
GusFU <guscp84@gmail.com>
2022-05-19 15:59:39 +0200

    tercera carga

--- promesas.js
+++ promesas.js
@@ -1,26 +1,39 @@
 //https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_9293.jpg
+
+/*function repe(raza1,raza2){
+    for(i=0;i<localStorage.length;i++){
+        if(raza1==raza2[i]){
+            return true 
+        }else{
+            return false
+        }
+    }
+
+    
+}*/
+
 var cont = 0
-var cont1 = 0
 function buscar() {
-
-
-
-
     fetch("https://dog.ceo/api/breeds/image/random")
         .then(res => res.json())
         .then(json => sacar(json))
-
 }
-
 function sacar(json) {
     var a = json.message
     localStorage.setItem(`perro${cont}`, json.message)
-    var mostrar = localStorage.getItem([0])
-
-    document.getElementById("foto").src = a
-    console.log(mostrar)
-    cont++
-    cont1++
+    let key;
+    //var mostrar = localStorage.getItem()
+    // https://images.dog.ceo/breeds/ovcharka-caucasian/IMG_20190811_105232.jpg
+    var razas = json.message.split("/")[4]
+    console.log(localStorage)
+    // for (let i = 0; i < localStorage.length-1; i++) {
+    //   key = localStorage.key(i);
+    //     var razasl = localStorage.getItem(key).split("/")[4]
+    //     console.log(i)
+    // }
+   
+    // document.getElementById("foto").src = a
+    // cont++
 }
 
 
